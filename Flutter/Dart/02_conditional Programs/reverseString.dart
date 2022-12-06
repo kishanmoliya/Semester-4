381980b668760d984ab69d6e4bdd20accdc08507
Kishan Moliya <94744840+kishanmoliya@users.noreply.github.com>
2022-12-06 10:55:08 +0530

    Update reverseString.dart

--- Flutter/Dart/02_conditional Programs/reverseString.dart
+++ Flutter/Dart/02_conditional Programs/reverseString.dart
@@ -1,1 +1,16 @@
-// Write a dart code to print the reverse string.
+//  Write a dart code to print the reverse string.
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter the String = ");
+  String? str = stdin.readLineSync()!;
+
+  String reverse = "";
+
+  for (int i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  print("reverse String = $reverse");
+  print(str.split('').reversed.join());
+}
