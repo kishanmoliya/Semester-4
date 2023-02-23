9ebe832b6bf8bedd27b4431c9b69fcaa8c222ed6
kishanmoliya <kishanmoliya232@gmail.com>
2023-02-23 19:09:21 +0530

    .

--- Flutter/flutter/construction_calculation/lib/assets/colors.dart
+++ Flutter/flutter/construction_calculation/lib/assets/colors.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
 
 // const primaryColor = Color(0xd5e50319);
-const primaryColor = Colors.purple;
+const primaryColor = Colors.pink;
+const deepPurple = Colors.deepPurple;
+const blue = Colors.blue;
+const lightGreen = Colors.lightGreen;
+const deepOrange = Colors.deepOrange;
+const greenAccent = Colors.greenAccent;
+const cyan = Colors.cyan;
+const grey = Colors.grey;
+
